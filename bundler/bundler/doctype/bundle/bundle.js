0c2b564989e7fabe40c469c952743eacffaba8a9
Tej Pochiraju <tej@iotready.co>
2021-05-22 14:08:04 +0530

    Added Bundle doctype and web form

---
+++ bundler/bundler/doctype/bundle/bundle.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Tej Pochiraju and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bundle', {
+	// refresh: function(frm) {
+
+	// }
+});
